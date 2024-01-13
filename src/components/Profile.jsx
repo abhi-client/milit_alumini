366fb61301522e5d8ca5b50a9504de4a154d4b3c
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2024-01-13 10:22:21 +0530

    :bug: sorted DSTSC

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+import AwesomeSlider from "react-awesome-slider";
+
 export default function Profile({
   batch = { title: "TSC", value: 23, id: 1 },
 }) {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const sub = ["ARMY", "AF", "NAVY"];
+  if (batch.title === "DSTSC")
+    return (
+      <>
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          {sub[activeSlide] == "AF" ? "AIR FORCE" : sub[activeSlide]}
+        </div>
+        <AwesomeSlider
+          selected={activeSlide}
+          bullets={false}
+          onTransitionEnd={(e) => {
+            setActiveSlide(e.currentIndex);
+          }}
+        >
+          {Array.apply(null, Array(3)).map((_, id) => (
+            <img
+              key={id}
+              src={`/images/${batch.title}/${batch.id}${sub[activeSlide]}.JPG`}
+              alt=""
+              style={{ objectFit: "contain", width: "100%", height: "88vh" }}
+            />
+          ))}
+        </AwesomeSlider>
+      </>
+    );
   return (
     <img
       src={`/images/${batch.title}/${batch.id}.JPG`}
